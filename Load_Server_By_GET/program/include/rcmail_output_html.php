0f704677f2533fbd60cfb9c265c8809edfe0387f
Octávio Filipe Pereira <58921861+octaviofilipepereira@users.noreply.github.com>
2020-01-15 16:08:15 +0000

    Update rcmail_output_html.php

--- Load_Server_By_GET/program/include/rcmail_output_html.php
+++ Load_Server_By_GET/program/include/rcmail_output_html.php
@@ -7,7 +7,7 @@ $available_hosts = $this->config->get('available_hosts');
 # Paste the follwing code inside this if
 // MagicBrain Hack 2020-01-14
 // Octavio Filipe Goncalves
-// Check if Client Server exists in Available Hosts
+// Check if we have server host by $GET
 if (in_array($_GET["clienthost"], $available_hosts)) {
     $input_host = new html_inputfield(array('name' => '_host', 'id' => 'rcmloginhost', 'value' => $_GET["clienthost"]) + $attrib + $host_attrib);
 } else {
@@ -19,7 +19,7 @@ if (in_array($_GET["clienthost"], $available_hosts)) {
 # Paste the follwing code inside this if
 // MagicBrain Hack 2020-01-14
 // Octavio Filipe Goncalves
-// Check if Client Server exists in Available Hosts
+// Check if Server Host exists in Available Hosts and perform needed actions
 if (in_array($_GET["clienthost"], $available_hosts)) {
     $button_attr = array('type' => 'submit', 'id' => 'rcmloginsubmit', 'class' => 'button mainaction submit');
     $out .= html::p('formbuttons', html::tag('button', $button_attr, $this->app->gettext('login')));
