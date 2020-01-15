c927ba2bff3ac0ba4c421868430a169f2e8fb71a
Octávio Filipe Pereira <58921861+octaviofilipepereira@users.noreply.github.com>
2020-01-15 16:02:32 +0000

    Create config.inc.php

---
+++ Load_Server_By_GET/config/config.inc.php
@@ -0,0 +1,8 @@
+# After $config['default_host']
+# Create an new array with the vailable hosts, that you want to permit
+
+$config['available_hosts'] = array(
+    'mail.test0.com',
+    'mail.test1.com',
+    'mail.test2.com',
+);
