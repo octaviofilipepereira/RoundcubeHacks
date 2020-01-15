635af55e552a670ea6ec382a39e11443d41359ec
Octávio Filipe Pereira <58921861+octaviofilipepereira@users.noreply.github.com>
2020-01-15 16:58:12 +0000

    Update config.inc.php

--- Load_Server_By_GET/config/config.inc.php
+++ Load_Server_By_GET/config/config.inc.php
@@ -1,6 +1,9 @@
 # After $config['default_host']
 # Create an new array with the vailable "available_hosts", that you want to permit
 
+// Roundcube Hack 2020-01-14
+// Octavio Filipe Goncalves
+// Create array available_hosts in config
 $config['available_hosts'] = array(
     'mail.test0.com',
     'mail.test1.com',
