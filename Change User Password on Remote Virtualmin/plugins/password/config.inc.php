0a0545e5531bb3d6b2ad08952dc478a87ed28837
Octávio Filipe Pereira <58921861+octaviofilipepereira@users.noreply.github.com>
2020-01-16 16:40:46 +0000

    Create config.inc.php

---
+++ Change User Password on Remote Virtualmin/plugins/password/config.inc.php
@@ -0,0 +1,13 @@
+// Password Plugin options
+// -----------------------
+// Change this value to virtualmin_remote, so you can load the correct driver
+$config['password_driver'] = 'virtualmin_remote';
+
+// Add this code in the bottom of the config file
+// VIRTUALMIN_REMOTE Driver options [Only for "virtualmin_remote" driver]
+// Define the user that have admin rights in remote your Webmin / Virtualmin servers (Usually: root)
+$config['vminruser'] = '';
+// Define your Webmin / Virtualmin port (Usually: 10000)
+$config['vminrport'] = '';
+// Define your AES key to decrypt Webmin / Virtualmin admin user password
+$config['virtualmin_DB_AES_key'] = '';
