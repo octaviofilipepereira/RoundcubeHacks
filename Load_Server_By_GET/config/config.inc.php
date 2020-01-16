e21f898099df861142e025eee9484ede22195a99
Octávio Filipe Pereira <58921861+octaviofilipepereira@users.noreply.github.com>
2020-01-16 11:19:58 +0000

    Update config.inc.php

--- Load_Server_By_GET/config/config.inc.php
+++ Load_Server_By_GET/config/config.inc.php
@@ -1,5 +1,7 @@
-# After $config['default_host']
-# Create an new array with the vailable "available_hosts", that you want to permit
+// ----------------------------------
+// IMAP - Permited Remote Servers
+// ----------------------------------
+// The IMAP hosts that are permited to perform the log-in.
 
 // Roundcube Hack 2020-01-14
 // Octavio Filipe Goncalves
@@ -9,3 +11,9 @@ $config['available_hosts'] = array(
     'mail.test1.com',
     'mail.test2.com',
 );
+
+// Show login button if IMAP host doesn't exist in array
+// Supported replacement variables:
+// 0 - don't show the login button
+// 1 - Show the login button disabled
+$config['show_login_button'] = '0';
